a99d6433b2c6cc0cf453670cd4237529b31c6ad1
Gledi Caushaj <gledi.alb@gmail.com>
2022-02-27 16:53:43 +0100

    Add secret page

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,6 @@ from pages import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("super-secret/", views.super_secret, name="secret"),
     path("pages/<page_name>/", views.PageView.as_view(), name="page"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
-from re import template
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import user_passes_test
 
 
 def home(request: HttpRequest) -> HttpResponse:
@@ -17,3 +17,8 @@ class PageView(TemplateView):
         page_name = self.kwargs.get("page_name", "home")
         template_name = page_name.lower().replace("-", "_").strip()
         return [f"pages/{template_name}.html"]
+
+
+@user_passes_test(lambda user: user.is_superuser)
+def super_secret(request):
+    return render(request, "pages/secret.html")
