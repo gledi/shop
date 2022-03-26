effe9ff110b513b19496667088608a3f293b2305
Gledi Caushaj <gledi.alb@gmail.com>
2022-03-26 11:16:21 +0100

    Add heroku

--- shop/settings.py
+++ shop/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "uploads/"
 MEDIA_ROOT = BASE_DIR / "uploads"
@@ -161,3 +164,6 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+
+django_heroku.settings(locals())
